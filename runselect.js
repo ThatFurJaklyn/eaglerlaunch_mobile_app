2ed5a71f8a167ad342822151b82ce570dfc11c27
ThatFurJaklyn <thejacksonburrito@gmail.com>
2024-09-16 00:29:35 -0400

    Create runselect.js
    this selects the link based on your selection. or it should. idk yet.

---
+++ runselect.js
@@ -0,0 +1,17 @@
+s = 'h2';
+function openTab(String l) {
+  window.open(l, "_blank");
+}
+if (s=1){
+  openTab("https://m.eaglercraft.com/mc/1.8.8/?userscript=irv77-eaglercraft-mobile.js")
+}
+if (s=2){
+  openTab("https://m.eaglercraft.com/mc/1.5.2/?userscript=irv77-eaglercraft-mobile.js")
+}
+if (s=3){
+  openTab("https://m.eaglercraft.com/mc/b1.7.3/?userscript=irv77-eaglercraft-mobile.js")
+}
+if (s=4){
+  openTab("https://m.eaglercraft.com/mc/indev/?userscript=irv77-eaglercraft-mobile.js")
+}
+
